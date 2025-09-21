607c4105630d8c520a4e22760002a1d2b2f4bf31
aayash <aayash@mail.com>
2025-09-21 15:03:40 +0300

    Changed the project structure and added a DatabseCOnnection Pool with basic unit tests

--- BuildHubInfrastructure/Class1.cs
+++ BuildHubInfrastructure/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace Core
+﻿namespace BuildHubInfrastructure
 {
     public class Class1
     {
---
+++ UnitTests/BuildHubDataEngineTests/DatabaseConnection/DatabaseConnectionPoolTests.cs
@@ -0,0 +1,15 @@
+﻿using BuildHubDataEngine.DatabaseConnection;
+
+namespace UnitTests.BuildHubDataEngineTests.DatabaseConnection
+{
+    [TestClass]
+    public sealed class DatabaseConnectionPoolTests
+    {
+        [TestMethod]
+        public void GetInstanceTest()
+        {
+            DatabaseConnectionPool databaseConnectionPoolInstance = DatabaseConnectionPool.GetInstance();
+            Assert.IsNotNull(databaseConnectionPoolInstance);
+        }
+    }
+}
