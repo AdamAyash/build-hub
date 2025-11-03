5173ff233c5b60f37544d1f63247bc1b6fef565f
Adam Ayash <aayash@mail.com>
2025-11-03 15:08:51 +0200

    Small chnage to the install python module BH-118

--- scripts/install_python_module.py
+++ scripts/install_python_module.py
@@ -1,3 +1,5 @@
+from utils import prompt_with_default
+
 def try_install_python_module(module_name):
     try:
         import importlib
@@ -5,6 +7,10 @@ def try_install_python_module(module_name):
         print(f"{module_name} is already installed.")
     except ImportError:
         import subprocess
-        print(f"{module_name} is not installed. Installing...")
+        print(f"{module_name} is not installed.")
+        confirmation = prompt_with_default("Do you want to install it? (y/n)", "y")
+        if(confirmation.lower() == "n"):
+            exit()
+                  
         subprocess.run(["pip", "install", module_name])
         print(f"{module_name} has been installed.")
