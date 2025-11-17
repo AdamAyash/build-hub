d0a490b906372be3e975dc642786794258014dca
sonikvvv <veselinnikolov11@gmail.com>
2025-11-17 16:16:39 +0200

    BH-137 Update Angular prefix and root selector to 'bh'
    Changed the Angular project prefix from 'app' to 'bh' in angular.json and updated the root component selector to 'bh-root' in app.component.ts for consistency with new naming conventions.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
+  selector: 'bh-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
