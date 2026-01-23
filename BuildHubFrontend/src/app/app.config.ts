3451b8478aa51efec38fa3ae48179de91c1bcba2
sonikvvv <veselinnikolov11@gmail.com>
2026-01-23 08:26:01 +0000

    BH-137 Updated modules
    Updated modules, added folder aliases and Ngrx packages.

--- BuildHubFrontend/src/app/app.config.ts
+++ BuildHubFrontend/src/app/app.config.ts
@@ -6,6 +6,10 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 import { routes } from './app.routes';
 
 import { providePrimeNG } from 'primeng/config';
@@ -17,11 +21,18 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+
+    // PrimeNG setup
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: myPreset,
       },
     }),
+
+    // NgRx global setup
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 50 }),
   ],
 };
